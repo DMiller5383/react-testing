1a69aee7837a12d7f3e54949a440c2fba12c112a
Daniel Miller <dmiller@cynosure.com>
2017-07-07 07:04:58 -0400

    first commit

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,15 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+//use describe to group together similar tests
+describe('App', () => {
+  //Use 'it' to test a single attribute of a target
+  it('shows the correct text', () => {
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+    //create an instance of App
 
-  it('renders something', () => {
-    expect(component).to.exist;
+    const component = renderComponent(App);
+    //Use expect to make an 'assertion' about a target
+    expect(component).to.contain('React simple starter');
   });
-});
+})
